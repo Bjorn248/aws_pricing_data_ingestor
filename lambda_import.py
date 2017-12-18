03e546398d093fe05d60ac63cdfeece6fa6830f8
Matthew Duren <matthewduren@gmail.com>
2017-12-18 15:44:09 -0500

    Update both functions to support python3 and more closely follow pep8 style. Tested working on local.

--- lambda_import.py
+++ lambda_import.py
@@ -1,33 +1,179 @@
+"""
+For downloading data from the AWS pricing API and importing into mysql/mariadb
+"""
+from __future__ import print_function
 import os
-import requests
 import hashlib
-import pymysql.cursors
 import csv
 import json
+import logging
+import requests
+import pymysql.cursors
 
 def lambda_handler(event, context):
-
-    if "MARIADB_HOST" in os.environ:
-        mariadb_host = os.environ["MARIADB_HOST"]
-    else:
-        mariadb_host = "localhost"
-
-    if "MARIADB_USER" in os.environ:
-        mariadb_user = os.environ["MARIADB_USER"]
-    else:
-        mariadb_user = "pricer"
-
-    if "MARIADB_PASSWORD" in os.environ:
-        mariadb_password = os.environ["MARIADB_PASSWORD"]
-    else:
-        mariadb_password = "prices123"
+    """Entrypoint function for aws lambda.  Event and context are unused."""
+    logging.basicConfig(level=logging.INFO)
+    column_titles = load_column_titles()
+    pricing_importer = PricingImporter(column_titles)
+    pricing_importer.main()
 
 
-    if "MARIADB_DB" in os.environ:
-        mariadb_db = os.environ["MARIADB_DB"]
-    else:
-        mariadb_db = "aws_prices"
+class PricingImporter(object):
+    """For importing aws pricing into a database"""
+    def __init__(self, column_titles):
+        self.column_titles = column_titles
+        # Pricing URLs
+        self.base_url = "https://pricing.us-east-1.amazonaws.com{}"
+        self.offer_index_url = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json"
+        # Retrieve environment variables
+        self.mariadb_host = os.getenv('MARIADB_HOST', 'localhost')
+        self.mariadb_user = os.getenv('MARIADB_USER', 'pricer')
+        self.mariadb_password = os.getenv('MARIADB_PASSWORD', 'prices123')
+        self.mariadb_db = os.getenv('MARIADB_DB', 'aws_prices')
 
+    @classmethod
+    def _md5(cls, file):
+        """Retrieves a md5 of the specified file."""
+        hash_md5 = hashlib.md5()
+        with open(file, 'rb') as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
+
+    @classmethod
+    def _download_file(cls, target_url, filename):
+        """Downloads a file from the specified URL"""
+        logging.info('Downloading file from %s...', target_url)
+        response = requests.get(target_url, stream=True)
+
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+
+    def _parse_csv_schema(self, filename, table_name):
+        """Parses a csv into schema"""
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[0] == "SKU":
+                    schema = self._generate_schema_from_row(row, table_name)
+                    return schema
+    def _generate_schema_from_row(self, row, table_name):
+        """Generate sql statement for schema"""
+        logging.info('Generating SQL Schema from CSV...')
+        schema_sql = 'create table {}(\n'.format(table_name)
+        for column_title in row:
+            if column_title in self.column_titles:
+                column_name = self.column_titles.get(column_title, {}).get('name', 'PARSING_ERROR')
+                column_type = self.column_titles.get(column_title, {}).get('type', 'PARSING_ERROR')
+                schema_sql += '{} {},\n'.format(column_name, column_type)
+            else:
+                schema_sql += ''.join(e for e in column_title if e.isalnum()) + " VARCHAR(200),\n"
+        schema_sql = schema_sql[:-2]
+        schema_sql += ");\n"
+        return schema_sql
+
+    def _download_offer_index_file(self, offer_index_filename):
+        """Downloads the offer index file"""
+        offer_index_exists = os.path.isfile(offer_index_filename)
+
+
+        if offer_index_exists:
+            resp = requests.head(self.offer_index_url)
+            md5_remote = resp.headers['etag'][1:-1]
+            if self._md5(offer_index_filename) == md5_remote:
+                logging.warning('You already have the latest offer index!')
+                return offer_index_exists
+        self._download_file(self.offer_index_url, offer_index_filename)
+        offer_index_exists = os.path.isfile(offer_index_filename)
+        return offer_index_exists
+
+    def load_offer_index(self):
+        """Loads the offer index from json file"""
+        offer_index_filename = "/tmp/offer_index.json"
+        offer_index_exists = self._download_offer_index_file(offer_index_filename)
+        if not offer_index_exists:
+            raise IOError('Failed to download offer index file!')
+
+        with open(offer_index_filename) as json_data:
+            offer_index = json.load(json_data)
+        return offer_index
+
+    def download_offer_file(self, offer_code_url):
+        """Downloads the offer file"""
+        offer_code = offer_code_url.split('/')[4]
+        offer_code_url = '{}.csv'.format(offer_code_url[:-5])
+        url = self.base_url.format(offer_code_url)
+        local_filename = '/tmp/{}.csv'.format(offer_code)
+
+        # Make sure the file does not already exist
+        file_exists = os.path.isfile(local_filename)
+
+        if file_exists:
+            resp = requests.head(url)
+            md5_remote = resp.headers['etag'][1:-1]
+            # If we already have the file, compare md5 of local and remote files
+            if self._md5(local_filename) == md5_remote:
+                logging.warning('You already have the latest csv for %s!  Skipping...', offer_code)
+                return file_exists
+        self._download_file(url, local_filename)
+
+        # Ensure the file now exists after downloading it
+        file_exists = os.path.isfile(local_filename)
+        return file_exists
+
+    def import_csv_into_mariadb(self, filename):
+        """Imports csv of data into mariadb"""
+        table_name = filename[:-4]
+        filename = '/tmp/{}'.format(filename)
+
+        schema = self._parse_csv_schema(filename, table_name)
+
+        db = pymysql.connect(host=self.mariadb_host,
+                             user=self.mariadb_user,
+                             passwd=self.mariadb_password,
+                             db=self.mariadb_db,
+                             local_infile=1)
+
+        cursor = db.cursor()
+        load_data = "LOAD DATA LOCAL INFILE '{}' INTO TABLE {}".format(filename, table_name)
+        load_data += """ FIELDS TERMINATED BY ','
+            ENCLOSED BY '"'
+        LINES TERMINATED BY '\n'
+        IGNORE 6 LINES; """
+
+        logging.info('Checking to see if table %s exists...', table_name)
+        cursor.execute("SELECT * FROM information_schema.tables WHERE table_schema = '{}' AND table_name = '{}' LIMIT 1;".format(self.mariadb_db, table_name))
+        if cursor.fetchone() is not None:
+            logging.info('Dropping existing table %s', table_name)
+            cursor.execute('DROP TABLE {};'.format(table_name))
+        logging.info('Recreating table...')
+        cursor.execute(schema)
+        logging.info('Loading csv data...')
+        cursor.execute(load_data)
+        db.commit()
+        cursor.close()
+
+    def main(self):
+        """Entrypoint function for class...downloads data and imports to mariadb"""
+        offer_index = self.load_offer_index()
+
+        filenames = []
+        urls = []
+        number_of_threads = 0
+        for offer, offer_info in offer_index.get('offers', {}).items():
+            number_of_threads += 1
+            filenames.append('{}.csv'.format(offer))
+            urls.append(offer_info.get('currentVersionUrl', 'PARSING_ERROR'))
+
+        for url in urls:
+            self.download_offer_file(url)
+
+        for filename in filenames:
+            self.import_csv_into_mariadb(filename)
+
+def load_column_titles():
+    """Nice place to store this until it can be loaded from a file"""
+    #TODO: Import this from a json file instead.
     column_titles = {
         "SKU": {
             "name": "SKU",
@@ -191,7 +337,7 @@ def lambda_handler(event, context):
         },
         "Group Description": {
             "name": "AWSGroupDescription",
-            "type": "VARCHAR(200)"
+            "type": "VARCHAR(300)"
         },
         "Transfer Type": {
             "name": "TransferType",
@@ -284,140 +430,14 @@ def lambda_handler(event, context):
         "Pre Installed S/W": {
             "name": "PreInstalledSW",
             "type": "VARCHAR(50)"
-         },
+        },
         "Processor Features": {
             "name": "ProcessorFeatures",
             "type": "VARCHAR(50)"
-         },
+        },
         "Sockets": {
             "name": "Sockets",
             "type": "VARCHAR(10)"
-         }
+        }
     }
-
-    def md5(file):
-        hash_md5 = hashlib.md5()
-        with open(file, 'rb') as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                hash_md5.update(chunk)
-        return hash_md5.hexdigest()
-
-    def download_file(targetURL, filename):
-        print("Downloading file from " + targetURL + "...\n")
-        response = requests.get(targetURL, stream=True)
-
-        with open(filename, 'wb') as f:
-            f.write(response.content)
-
-    def parse_csv_schema(filename, table_name):
-        with open(filename, 'rb') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                if row[0] == "SKU":
-                    return generate_schema_from_row(row, table_name)
-                    break
-
-    def generate_schema_from_row(row, table_name):
-        print "Generating SQL Schema from CSV..."
-        schema_sql = "create table " + table_name + "(\n"
-        for column_title in row:
-            if column_title in column_titles:
-                schema_sql += column_titles[column_title]['name'] + ' ' + column_titles[column_title]['type'] + ",\n"
-            else:
-                schema_sql += ''.join(e for e in column_title if e.isalnum()) + " VARCHAR(200),\n"
-        schema_sql = schema_sql[:-2]
-        schema_sql += ");\n"
-        return schema_sql
-
-    def download_offer_file(offer_code_url):
-
-        offer_code = offer_code_url.split('/')[4]
-
-        base_url = "https://pricing.us-east-1.amazonaws.com"
-
-        offer_code_url = offer_code_url[:-5] + ".csv"
-
-        url = base_url + offer_code_url
-
-        local_filename = "/tmp/" + offer_code + ".csv"
-
-        file_exists = os.path.isfile(local_filename)
-
-        if file_exists:
-            resp = requests.head(url)
-            md5_remote = resp.headers['etag'][1:-1]
-            if md5(local_filename) == md5_remote:
-                print("You already have the latest csv for " + offer_code + "!")
-            else:
-                download_file(url, local_filename)
-        else:
-            download_file(url, local_filename)
-
-        file_exists = os.path.isfile(local_filename)
-
-    def import_csv_into_mariadb(filename):
-
-        table_name = filename[:-4]
-
-        filename = "/tmp/" + filename
-
-        schema = parse_csv_schema(filename, table_name)
-
-        db = pymysql.connect(host=mariadb_host,
-                              user=mariadb_user,
-                              passwd=mariadb_password,
-                              db=mariadb_db,
-                              local_infile=1)
-
-        cursor = db.cursor()
-        load_data = "LOAD DATA LOCAL INFILE '" + filename + "' INTO TABLE " + table_name
-        load_data += """ FIELDS TERMINATED BY ','
-            ENCLOSED BY '"'
-        LINES TERMINATED BY '\n'
-        IGNORE 6 LINES; """
-
-        print "Checking to see if table " + table_name + " exists..."
-        cursor.execute("SELECT * FROM information_schema.tables WHERE table_schema = '" + mariadb_db + "' AND table_name = '" + table_name + "' LIMIT 1;")
-        if cursor.fetchone() is not None:
-            print "Dropping existing table " + table_name
-            cursor.execute("DROP TABLE " + table_name + ";")
-        print "Recreating table..."
-        cursor.execute(schema)
-        print "Loading csv data..."
-        cursor.execute(load_data)
-        db.commit()
-        cursor.close()
-
-    offer_index_filename = "/tmp/offer_index.json"
-
-    offer_index_exists = os.path.isfile(offer_index_filename)
-
-    offer_index_url = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json"
-
-    if offer_index_exists:
-        resp = requests.head(offer_index_url)
-        md5_remote = resp.headers['etag'][1:-1]
-        if md5(offer_index_filename) == md5_remote:
-            print("You already have the latest offer index!")
-        else:
-            download_file(offer_index_url, offer_index_filename)
-    else:
-        download_file(offer_index_url, offer_index_filename)
-
-
-    with open(offer_index_filename) as json_data:
-        offer_index = json.load(json_data)
-
-    filenames = []
-    urls = []
-    number_of_threads = 0
-    for offer, offer_info in offer_index['offers'].iteritems():
-        number_of_threads += 1
-        filenames.append(offer + ".csv")
-        urls.append(offer_info['currentVersionUrl'])
-
-    for url in urls:
-        download_offer_file(url)
-
-    for filename in filenames:
-        import_csv_into_mariadb(filename)
+    return column_titles
